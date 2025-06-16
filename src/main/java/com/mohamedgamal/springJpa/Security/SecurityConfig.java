a844b6dd0004030594bbd7c30e7baa4979fb5ab3
Mohamedgamaal16 <mahooomar@yahoo.com>
2025-06-16 22:00:28 +0300

    add basic securtity

---
+++ src/main/java/com/mohamedgamal/springJpa/Security/SecurityConfig.java
@@ -0,0 +1,60 @@
+package com.mohamedgamal.springJpa.Security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(auth -> {
+            auth.requestMatchers("/api/books/**").permitAll(); // allow public access
+            auth.requestMatchers("/h2-console/**").permitAll(); // allow H2 console access
+            auth.requestMatchers("/api/author/**").hasRole("ADMIN"); // only ADMIN
+            auth.anyRequest().authenticated(); // secure other endpoints
+        });
+
+        // Disable CSRF for H2 Console (needed for H2 console to work)
+        http.csrf(csrf -> csrf.ignoringRequestMatchers("/h2-console/**"));
+
+        // Allow frames for H2 Console (H2 console uses frames)
+        http.headers(headers -> headers.frameOptions(frameOptions -> frameOptions.sameOrigin()));
+        http.formLogin(Customizer.withDefaults());
+        http.httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails admin = User.builder()
+                .username("mohamed")
+                .password(passwordEncoder().encode("1234"))
+                .roles("ADMIN")
+                .build();
+
+        UserDetails user = User.builder()
+                .username("samir")
+                .password(passwordEncoder().encode("1234"))
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(admin, user);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
